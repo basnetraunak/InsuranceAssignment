362c1936b34e4be995715687bd5e55d65a769308
RaunakBasnet <Rrb3493@gmail.com>
2025-03-02 15:20:56 +1100

    feat: take input from user

---
+++ CommandProcessor.cs
@@ -0,0 +1,18 @@
+namespace InsuranceWAAssignment
+{
+    class CommandProcessor
+    {
+        public static void Procress(string userInput)
+        {
+
+            int inputlength = userInput.Length;
+            if (inputlength > 6)
+            {
+                //Validate.PlaceCommandchecker(userInput);
+                return;
+            }
+
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using InsuranceWAAssignment;
+namespace InsuranceofWAAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--------------------------------------------------------------- Program Launched ----------------------------------------------------------------------------------------------");
+
+            Console.WriteLine("Please enter a PLACE command \n (FORMAT: PLACE X,Y,FACING):");
+            string? userInput = Console.ReadLine();
+            if (!string.IsNullOrWhiteSpace(userInput))
+            {
+                CommandProcessor.Procress(userInput);
+            }
+            else { Console.WriteLine("Sorry, Input cannot be empty!"); }
+
+
+
+        }
+    }
+}
